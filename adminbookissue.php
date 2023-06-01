74fedb7b953edb7d2a51caf917fe94b744ffe5c6
sam-cudo <rahularjun32@gmail.com>
2023-06-01 10:19:36 +0530

    Added Dynamic dropdowns and Issued book table
    1. Dynamic dropdowns(Member ID, Book ID) fetch there options from database.
    2. With the help of session storage the selected values are retained after page refresh
    3. Based on selection from dropdowns two other inputs(Member Name, Book Name) are auto populated from database

--- adminbookissue.php
+++ adminbookissue.php
@@ -1,6 +1,107 @@
+<?php 
+
+    $errors = array('issueDate' => '','returnDate' => '', 'memberId' => '', 'bookId' => '');
+    $issueDate = $returnDate = $bookRow['book_name'] = $memberRow['full_name'] = '';
+    include('conn.php');
+
+//Fetching option values for Member ID dropdown    
+    $sql = 'SELECT member_id FROM member_master_tbl';
+    $stmt = sqlsrv_query($conn, $sql);
+    $memberOptions = array();
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $memberOptions[] = $row;
+    }
+
+//Fetching option values for Book ID dropdown    
+    $sql = 'SELECT book_id FROM book_master_tbl';
+    $stmt = sqlsrv_query($conn, $sql);
+    $bookOptions = array();
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $bookOptions[] = $row;
+    }
+
+
+    if (isset($_POST['Submit'])) {
+        $selectedMember = $_POST["MemberID"];
+        $selectedBook = $_POST["BookID"];
+    
+        // Validate the selected option
+        if (empty($selectedOption)) {
+            $errorMessage = "Please select an option.";
+        } else {
+
+        }
+    }
+
+//Fetch the Member Name whenever a Member ID is selected from dropdown
+    if (isset($_GET['selectedMember'])) {
+        $selectedMember = $_GET["selectedMember"];
+        $sql = "SELECT full_name FROM member_master_tbl WHERE member_id = '$selectedMember' ;";
+        $stmt = sqlsrv_query($conn, $sql);
+        if( $stmt === false ) 
+            die( print_r( sqlsrv_errors(), true));
+
+            $memberRow = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC );
+    }
+
+//Fetch the Book Name whenever a Book ID is selected from dropdown    
+    if (isset($_GET['selectedBook'])) {
+        $selectedBook = $_GET["selectedBook"];
+        $sql = "SELECT book_name FROM book_master_tbl WHERE book_id = '$selectedBook' ;";
+        $stmt = sqlsrv_query($conn, $sql);
+        if( $stmt === false ) 
+            die( print_r( sqlsrv_errors(), true));
+
+            $bookRow = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC );       
+    }
+
+
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn);
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html>
+
     <?php include('templates/header.php'); ?>
+
+    <script>
+        $(document).ready(function() {
+
+        //To retain the selected values After page refresh
+            var storedMember = sessionStorage.getItem("selectedMember");
+            var selectedMember = storedMember;
+            if (storedMember) {
+                document.getElementById("MemberID").value = storedMember;
+            }
+
+            var storedBook = sessionStorage.getItem("selectedBook");
+            var selectedBook = storedBook;
+            if (storedBook) {
+                document.getElementById("BookID").value = storedBook;
+            }
+
+        //Send a get request to server whenever selected value changes of dropdowns
+            $('#MemberID').change(function() {
+                selectedMember = $(this).val();
+                sessionStorage.setItem("selectedMember", selectedMember);
+                url = "adminbookissue.php?selectedMember=" + encodeURIComponent(selectedMember) + "&selectedBook=" + encodeURIComponent(selectedBook);
+                window.location.href = url;
+            });
+
+            $('#BookID').change(function() {
+                selectedBook = $(this).val();
+                sessionStorage.setItem("selectedBook", selectedBook);
+                url = "adminbookissue.php?selectedMember=" + encodeURIComponent(selectedMember) + "&selectedBook=" + encodeURIComponent(selectedBook);
+                window.location.href = url;
+            });
+        });
+    </script>
+
     <?php include('templates/sidebar.php'); ?>
     <div class="content" id="content">
         <button class="toggleButton" id="toggleButton"></button>
@@ -69,16 +170,22 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Member ID</label>
-                                        <select class="form-control" name="MemberID">
+                                        <select class="form-control" name="MemberID" id="MemberID">
                                             <option value="">Select Member ID</option>
+                                            <?php foreach ($memberOptions as $option) : ?>
+                                                <option value="<?php echo $option['member_id']; ?>"><?php echo $option['member_id']; ?></option>
+                                            <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Book ID</label>
-                                        <select class="form-control" name="BookID">
+                                        <select class="form-control" name="BookID" id="BookID">
                                             <option value="">Select Book ID</option>
+                                            <?php foreach ($bookOptions as $option) : ?>
+                                                <option value="<?php echo $option['book_id']; ?>"><?php echo $option['book_id']; ?></option>
+                                            <?php endforeach; ?>
                                         </select>
                                     </div>
                                 </div>
@@ -87,13 +194,13 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Member Name</label>
-                                        <input class="form-control" type="text" name="MemberName" placeholder="Member Name" value="<?php echo $memberName; ?>" readonly></input>
+                                        <input class="form-control" type="text" name="MemberName" placeholder="Member Name" value="<?php echo $memberRow['full_name']; ?>" readonly></input>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label>Book Name</label>
-                                        <input class="form-control" type="text" name="BookName" placeholder="Book Name" value="<?php echo $bookName; ?>" readonly></input>
+                                        <input class="form-control" type="text" name="BookName" placeholder="Book Name" value="<?php echo $bookRow['book_name']; ?>" readonly></input>
                                     </div>
                                 </div>
                             </div>
@@ -147,7 +254,47 @@
                         </div>
                         <div class="row">
                             <div class="col">
-
+                                <?php 
+                                    include('conn.php');
+                                    $sql = "SELECT * FROM book_issue_tbl;";
+                                    $stmt = sqlsrv_query($conn, $sql);    
+                                ?>
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr class="table-success">
+                                            <th scope="col">Member ID</th>
+                                            <th scope="col">Member Name</th>                                            
+                                            <th scope="col">Book ID</th>
+                                            <th scope="col">Book Name</th>
+                                            <th scope="col">Issue Date</th>
+                                            <th scope="col">Due Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC )): ?>
+                                        <!-- Logic for deafulters (red color) -->
+                                        <?php $date = date('Y-m-d', time());
+                                            if($row['due_date'] > $date): ?>
+                                                <tr>
+                                            <?php endif; ?>
+                                        <?php $date = date('Y-m-d', time());
+                                            if($row['due_date'] < $date): ?>
+                                                <tr class="table-danger">
+                                        <?php endif; ?>
+                                            <td><?php echo htmlspecialchars($row['member_id']) ?></td>
+                                            <td><?php echo htmlspecialchars($row['member_name']) ?></td>
+                                            <td><?php echo htmlspecialchars($row['book_id']) ?></td>
+                                            <td><?php echo htmlspecialchars($row['book_name']) ?></td>
+                                            <td><?php echo htmlspecialchars($row['issue_date']) ?></td>
+                                            <td><?php echo htmlspecialchars($row['due_date']) ?></td>
+                                        </tr>
+                                        <?php endwhile; ?>
+                                    </tbody>
+                                </table>
+                                <?php 
+                                    sqlsrv_free_stmt($stmt);
+                                    sqlsrv_close($conn);
+                                ?>
                             </div>
                         </div>
                     </div>
